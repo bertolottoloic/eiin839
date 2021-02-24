f085b26210578cd0a4c3a94b673862930b6dcd0d
bertolottoloic <loic.bertolotto@etu.unice.fr>
2021-02-24 15:38:06 +0100

    question 1

---
+++ TD3/SolutionTD3/ProjetTD3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Ques1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient http = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await http.GetAsync("https://api.jcdecaux.com/vls/v3/stations?apiKey=bcf8e0014e5509ec4e4238d062207b525253b599");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+                Console.ReadLine();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+        }
+
+    }
+}
