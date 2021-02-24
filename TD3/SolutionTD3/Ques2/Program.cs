05ce68c504c986eb31832021a876fcf870200ad8
bertolottoloic <loic.bertolotto@etu.unice.fr>
2021-02-24 16:45:09 +0100

    debut ques 2

--- TD3/SolutionTD3/Ques2/Program.cs
+++ TD3/SolutionTD3/Ques2/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace Ques2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            HttpClient http = new HttpClient();
+            try
+            {
+
+                System.Collections.Generic.List<Station> stations = await http.GetFromJsonAsync<System.Collections.Generic.List<Station>>("https://api.jcdecaux.com/vls/v3/stations?apiKey=bcf8e0014e5509ec4e4238d062207b525253b599");
+                stations.ForEach(station => Console.WriteLine(station));
+                Console.ReadLine();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
         }
     }
 }
---
+++ TD3/SolutionTD3/Ques2/Station.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques2
+{
+    class Station
+    {
+        int number;
+        string contractName;
+        string name;
+        string address;
+        int[] position;
+        bool banking;
+        bool bonus;
+        string status;
+        string lastUpdate;
+        bool connected;
+        bool overflow;
+        string shape;
+        Couple totalStands;
+        Couple mainStands;
+        Couple overflowStands;
+
+        public int Number { get => number; set => number = value; }
+        public string ContractName { get => contractName; set => contractName = value; }
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public int[] Position { get => position; set => position = value; }
+        public bool Banking { get => banking; set => banking = value; }
+        public bool Bonus { get => bonus; set => bonus = value; }
+        public string Status { get => status; set => status = value; }
+        public string LastUpdate { get => lastUpdate; set => lastUpdate = value; }
+        public bool Connected { get => connected; set => connected = value; }
+        public bool Overflow { get => overflow; set => overflow = value; }
+        public string Shape { get => shape; set => shape = value; }
+        internal Couple TotalStands { get => totalStands; set => totalStands = value; }
+        internal Couple MainStands { get => mainStands; set => mainStands = value; }
+        internal Couple OverflowStands { get => overflowStands; set => overflowStands = value; }
+    }
+
+    class Couple
+    {
+        int[] availabilities;
+        int capacity;
+
+        public int[] Availabilities { get => availabilities; set => availabilities = value; }
+        public int Capacity { get => capacity; set => capacity = value; }
+    }
+
+    
+}
