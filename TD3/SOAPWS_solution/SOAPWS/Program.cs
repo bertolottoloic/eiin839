c7b652a115425b3b14b64a086c1348028e92e47a
Bertolotto Loïc <34398393+bertolottoloic@users.noreply.github.com>
2021-03-10 16:03:50 +0100

    add soap web client

---
+++ TD3/SOAPWS_solution/SOAPWS/Program.cs
@@ -0,0 +1,15 @@
+﻿using ServiceReference1;
+using System;
+
+namespace SOAPWS
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CalculatorSoap calculator = 
+            System.Threading.Tasks.Task<int> p = await CalculatorSoap.AddAsync(3, 2);
+        }
+    }
+}
---
+++ TD3/SOAPWS_solution/WSSOAP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSSOAP.ServiceReference1;
+
+namespace WSSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorSoapClient calculator = new CalculatorSoapClient();
+            Console.WriteLine(calculator.Add(3, 12));
+            Console.ReadLine();
+        }
+    }
+}
